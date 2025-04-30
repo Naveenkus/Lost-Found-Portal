dfeae0ccaed79e83c69fdc6d4ac2cec71f98d544
Naveen Kushwaha <naveenkushwah024@gmail.com>
2025-04-30 21:22:00 +0530

    Initial commit

---
+++ src/test/java/com/example/lostoria/LostoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lostoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
